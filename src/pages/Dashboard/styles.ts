c649a84e32e81a89bf8e08e9913b816e80a6fad2
ian <iancmilan@unochapeco.edu.br>
2021-01-22 17:47:46 -0300

    estilizando dashboard e conectando api 🚀

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -63,5 +63,42 @@ export const Repositories = styled.div`
 
     display: flex;
     align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      // com o flex 1 a div vai se ajustar com o tamanho que ela tem disponível
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
   }
 `;
